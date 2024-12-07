ae03aafadf336394a06cdf4f22a4203cc38836fd
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-07 10:23:52 +0400

    handleRequest

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,13 @@ const App = () => {
   const [loading , setLoading] = useState(false) 
   const [data , setData] = useState({}) 
   const [err , setErr] = useState(false) 
+  const handleRequest = () => {
+    setLoading(true)
+  }
   return (
     <>
   <h1>UseReducer</h1>
+  <button onClick={handleRequest}>send request</button>
   <h2>-------------------------------------</h2>
   <h2>Loading {loading ? "true" : "false"}</h2>
   <h2>data {JSON.stringify(data) == "{}" ? "Empty" : "Full"}</h2>
